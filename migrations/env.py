34bb0a4849879523276b1ad75161105ad4347db7
MrJohnDev <williampuh@gmail.com>
2023-12-24 12:32:47 +0700

    init

--- migrations/env.py
+++ migrations/env.py
@@ -5,6 +5,13 @@ from sqlalchemy import pool
 
 from alembic import context
 
+import sys
+from pathlib import Path
+# Это добавляет путь к корневому каталогу проекта в sys.path
+sys.path.append(str(Path(__file__).resolve().parents[1]))
+
+from models import Base  # Импорт вашей модели
+
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
@@ -18,7 +25,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
